1dfa21de12aedd327fc54a320d5d4831326493e8
Matt Miller <thisismattmiller@gmail.com>
2015-09-17 14:18:05 -0400

    mms ingest

---
+++ jobs/mms_ingest.js
@@ -0,0 +1,67 @@
+"use strict"
+
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+	cluster.fork()
+	cluster.fork()
+	cluster.fork()
+	cluster.fork()
+
+
+	setInterval(function(){
+		if (Object.keys(cluster.workers).length === 0){
+			process.exit()
+		}
+	},30000)
+
+
+} else {
+
+
+
+
+	if (cluster.worker.id == 1){
+
+		var mmsCollectionIngest = require("../lib/mss_ingest_collections.js")
+		mmsCollectionIngest.ingest(function(err,results){
+			console.log("done")
+
+			cluster.worker.disconnect()
+
+
+		})
+
+
+
+	}else if (cluster.worker.id == 2){
+
+		var mmsContainerIngest = require("../lib/mss_ingest_containers.js")
+		mmsContainerIngest.ingest(function(err,results){
+			console.log("done")
+			cluster.worker.disconnect()
+		})
+
+
+	}else if (cluster.worker.id == 3){
+
+		var mmsItemsIngest = require("../lib/mss_ingest_items.js")
+		mmsItemsIngest.ingest(function(err,results){
+			console.log("done")
+			cluster.worker.disconnect()
+		})
+
+	}else if (cluster.worker.id == 4){
+
+
+		var mmsCapturesIngest = require("../lib/mss_ingest_captures.js")
+		mmsCapturesIngest.ingest(function(err,results){
+			console.log("done")
+			cluster.worker.disconnect()
+		})
+
+	}
+
+
+}
