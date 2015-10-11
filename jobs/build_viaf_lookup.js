063cf01eb057bab08efac55c8dce8f21e0c7b907
Matt Miller <thisismattmiller@gmail.com>
2015-10-11 11:23:49 -0400

    viaf data load

--- jobs/build_viaf_lookup.js
+++ jobs/build_viaf_lookup.js
@@ -1,5 +1,7 @@
 "use strict"
 
+//This script will need like 20GB of ram, because it stores all the data internally for the final output.
+
 var config = require("config")
 var fs = require("fs")
 var byline = require('byline')
@@ -17,149 +19,89 @@ var viafExtract =  __dirname + '/..' + config['Source']['viafExtract']
 var allLookup = {}
 var count = 0
 
-//db.prepareViafLookup(function(){
 
-	// db.returnCollection("viafLookup",function(err,viafLookup,database){
+var stream = fs.createReadStream(viafExtract, { encoding: 'utf8' });
+stream = byline.createStream(stream);
 
-		var stream = fs.createReadStream(viafExtract, { encoding: 'utf8' });
-		stream = byline.createStream(stream);
-
-		stream.on('data', function(line) {
+stream.on('data', function(line) {
 
 
 
-			process.stdout.cursorTo(0)
-			process.stdout.write(clc.black.bgYellowBright("build viaf: " + ++count ))
+	process.stdout.cursorTo(0)
+	process.stdout.write(clc.black.bgYellowBright("build viaf: " + ++count ))
 
-			stream.pause()
+	stream.pause()
 
-			parser.parse(line, function (error, triple, prefixes) {
+	parser.parse(line, function (error, triple, prefixes) {
 
-				//this is an iterator, but since we are only processing one triple, it will one fire once
-				if (triple){
+		//this is an iterator, but since we are only processing one triple, it will one fire once
+		if (triple){
 
-					if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#prefLabel' || triple.predicate == 'http://www.w3.org/2004/02/skos/core#altLabel' || triple.predicate == 'http://xmlns.com/foaf/0.1/focus'){
-						
-						// //see if we have this one yet
-						// viafLookup.find({ _id: triple.object.toString() }).toArray(function(err, existingRecord) {
-
-						// 	if (existingRecord.length==0){
-						// 		var updateRecord = {
-						// 			_id: triple.subject.toString(),
-						// 			prefLabel : "",
-						// 			altLabel : [],
-						// 			viaf: 0,
-						// 			normalized: []
-						// 		}
-						// 	}else{
-						// 		var updateRecord = existingRecord[0]
-						// 		console.log("ALready exist!")
-						// 		console.log(updateRecord)
-						// 	}
-
-
-
-							// if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#prefLabel'){
-							// 	updateRecord.prefLabel = N3Util.getLiteralValue(triple.object)
-
-							// 	updateRecord.normalized.push(utils.normalizeAndDiacritics(N3Util.getLiteralValue(triple.object)))
-
-
-							// }else if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#altLabel'){
-							// 	updateRecord.altLabel.push( N3Util.getLiteralValue(triple.object))
-
-							// 	updateRecord.normalized.push(utils.normalizeAndDiacritics(N3Util.getLiteralValue(triple.object)))
-
-							// }else if (triple.predicate == 'http://xmlns.com/foaf/0.1/focus'){
-							// 	updateRecord.viaf = parseInt(triple.object.toString().split('/viaf/')[1])
-							// }
-
-							// //we need to update
-							// viafLookup.update({ _id : updateRecord._id }, { $set: updateRecord }, { upsert: true} , function(err, result) {
-							// 	if (err) console.log(err)		
-							// 	stream.resume()
-
-							// 	return true
-
-							// })
-
-
-						// 	return true
-						// })			
-							
-							var key = triple.subject.toString().split('http://viaf.org/viaf/sourceID/LC%7C')[1].replace("#skos:Concept",'')
-
-
-							if (!allLookup[key]){
-
-
-								allLookup[key] = {
-									prefLabel : "",
-									altLabel : [],
-									viaf: 0,
-									normalized: []
-								}
-								
-							}
-
-							if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#prefLabel'){
-								var literal = N3Util.getLiteralValue(triple.object)
-								allLookup[key].prefLabel = literal
-
-								allLookup[key].normalized.push(utils.normalizeAndDiacritics(literal))
-
-
-							}else if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#altLabel'){
-								var literal = N3Util.getLiteralValue(triple.object)
-								allLookup[key].altLabel.push( literal)
-
-								allLookup[key].normalized.push(utils.normalizeAndDiacritics(literal))
-
-							}else if (triple.predicate == 'http://xmlns.com/foaf/0.1/focus'){
-								allLookup[key].viaf = parseInt(triple.object.toString().split('/viaf/')[1])
-							}
-							
-						
-
-							
-
-							stream.resume()
-							return true
-
-					}else{
-						stream.resume()
-						return true
-					}				
+			if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#prefLabel' || triple.predicate == 'http://www.w3.org/2004/02/skos/core#altLabel' || triple.predicate == 'http://xmlns.com/foaf/0.1/focus'){
 				
-				}	
+					var key = triple.subject.toString().split('http://viaf.org/viaf/sourceID/LC%7C')[1].replace("#skos:Concept",'')
 
-			})	
-		})
+					if (!allLookup[key]){
 
+						allLookup[key] = {
+							prefLabel : "",
+							altLabel : [],
+							viaf: 0,
+							normalized: []
+						}
+						
+					}
 
+					if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#prefLabel'){
+						var literal = N3Util.getLiteralValue(triple.object)
+						allLookup[key].prefLabel = literal
 
-		stream.on('end', function () {
-			console.log("Doneeeeee")
-
-
-			var file = fs.createWriteStream('viafLookupData.txt');
-			file.on('error', function(err) { console.lof(err) })
-			var writeCounter = 0
-
-			for (var x in allLookup){
-				file.write(JSON.stringify(allLookup[x]) + "\n")
-				process.stdout.cursorTo(50)
-				process.stdout.write(clc.black.bgBlueBright("write viaf: " + ++writeCounter ))
-
-			}
-			file.end();	
-
-			
-			//database.close()
-		})
-
-	//})
-
-//})
+						allLookup[key].normalized.push(utils.normalizeAndDiacritics(literal))
 
 
+					}else if (triple.predicate == 'http://www.w3.org/2004/02/skos/core#altLabel'){
+						var literal = N3Util.getLiteralValue(triple.object)
+						allLookup[key].altLabel.push( literal)
+
+						allLookup[key].normalized.push(utils.normalizeAndDiacritics(literal))
+
+					}else if (triple.predicate == 'http://xmlns.com/foaf/0.1/focus'){
+						allLookup[key].viaf = parseInt(triple.object.toString().split('/viaf/')[1])
+					}
+					
+				
+
+					
+
+					stream.resume()
+					return true
+
+			}else{
+				stream.resume()
+				return true
+			}				
+		
+		}	
+
+	})	
+})
+
+
+
+stream.on('end', function () {
+	console.log("Doneeeeee")
+
+
+	var file = fs.createWriteStream('viafLookupData.txt');
+	file.on('error', function(err) { console.lof(err) })
+	var writeCounter = 0
+
+	for (var x in allLookup){
+		file.write(JSON.stringify(allLookup[x]) + "\n")
+		process.stdout.cursorTo(50)
+		process.stdout.write(clc.black.bgBlueBright("write viaf: " + ++writeCounter ))
+
+	}
+	file.end();	
+
+})
---
+++ jobs/insert_viaf_data.js
@@ -0,0 +1,64 @@
+"use strict"
+
+var config = require("config")
+var fs = require("fs")
+var byline = require('byline')
+var N3 = require('n3')
+var db = require("../lib/db.js")
+var utils = require("../lib/utils.js")
+var clc = require('cli-color')
+
+
+var viafExtract =  __dirname + '/..' + config['Source']['viafExtractInsert']
+
+var allLookup = {}
+var count = 0
+
+db.prepareViafLookup(function(){
+
+	db.returnCollection("viafLookup",function(err,viafLookup,database){
+
+		var stream = fs.createReadStream(viafExtract, { encoding: 'utf8' });
+		stream = byline.createStream(stream);
+
+		stream.on('data', function(line) {
+
+			stream.pause()
+
+
+
+			process.stdout.cursorTo(0)
+			process.stdout.write(clc.black.bgYellowBright("insert viaf: " + ++count ))
+
+			var updateRecord = JSON.parse(line)
+
+			updateRecord._id = updateRecord.viaf
+			delete updateRecord.viaf
+
+			viafLookup.update({ _id : updateRecord._id }, { $set: updateRecord }, { upsert: true} , function(err, result) {
+				
+				if (err) console.log(err)
+				return true
+
+			})		
+
+			stream.resume()
+
+		})
+
+		stream.on('end', function () {
+
+
+			console.log("Doneeeeee")	
+			setTimeout(function(){
+
+				database.close()
+			},30000)		
+			
+		})
+
+	})
+
+})
+
+
