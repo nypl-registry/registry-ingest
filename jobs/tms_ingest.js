72f3cf6bb0eb6a2256ac0377c75d53bb07fcf35e
Matt Miller <thisismattmiller@gmail.com>
2015-10-14 00:10:31 -0400

    serialize agents viaf

--- jobs/tms_ingest.js
+++ jobs/tms_ingest.js
@@ -4,11 +4,21 @@
 var tmsObjectsIngest = require("../lib/tms_ingest_objects.js")
 
 
-tmsObjectsIngest.ingest(function(err,results){
+// tmsObjectsIngest.ingest(function(err,results){
 
-	console.log("Ingesting tms objects done")
-	//cluster.worker.disconnect()
-})
+// 	console.log("Ingesting tms objects done")
+
+	tmsObjectsIngest.reconcileUlan(function(err,results){
+
+		
+
+		console.log("reconcileUlan tms objects done")
+		//cluster.worker.disconnect()
+	})
+
+
+
+// })
 
 
 
