6b30edfa9c5318bc7d9e8f9d2061b6dfe6e58cd8
Matt Miller <thisismattmiller@gmail.com>
2015-11-09 12:03:16 -0500

    tms mapping shadowcat material type k lookup

---
+++ jobs/map_tms_setup.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+var tmsStrategies = require("../lib/tms_mapping_strategies.js")
+
+
+tmsStrategies.buildShadowcatMaterialKLookup(function(){
+
+
+
+
+	console.log("done")
+
+})
+
+
