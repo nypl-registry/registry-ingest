2bf2e7dfc4fcd2b91156f33710b396a4fe0d04aa
Matt Miller <thisismattmiller@gmail.com>
2015-11-16 12:45:44 -0500

    viaf into mms item fix

--- jobs/serialize_mongo_agents_mms_tms_archives.js
+++ jobs/serialize_mongo_agents_mms_tms_archives.js
@@ -4,19 +4,19 @@
 var serialize = require("../lib/serialize_utils.js")
 
 
-// serialize.populateArchivesAgentsCollctions(function(){	
-// 	serialize.populateArchivesAgentsComponents(function(){	
-// 		serialize.populateTmsAgents(function(){
-// 			serialize.populateMmsAgentsCollections(function(){
-// 				serialize.populateMmsAgentsContainers(function(){
+serialize.populateArchivesAgentsCollctions(function(){	
+	serialize.populateArchivesAgentsComponents(function(){	
+		serialize.populateTmsAgents(function(){
+			serialize.populateMmsAgentsCollections(function(){
+				serialize.populateMmsAgentsContainers(function(){
 					serialize.populateMmsAgentsItems(function(){
 
 					})
-// 				})
-// 			})
-// 		})
-// 	})
-// })
+				})
+			})
+		})
+	})
+})
 
 
 
