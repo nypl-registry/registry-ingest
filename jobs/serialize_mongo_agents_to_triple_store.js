71c55a054243cdab82c1de415cdfb4a01856bf13
Matt Miller <thisismattmiller@gmail.com>
2016-01-18 16:27:57 -0500

    insert agent triples

---
+++ jobs/serialize_mongo_agents_to_triple_store.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var serializeTripleStoreUtil = require("../lib/serialize_triple_store_utils.js")
+
+
+serializeTripleStoreUtil.prepareAgents(function(){
+
+
+
+	
+	serializeTripleStoreUtil.insertAgents(function(){
+		process.exit()		
+	})
+
+
+
+
+})
