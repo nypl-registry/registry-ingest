cf3d09a7d230fad160227963d54b0bfb23c49f0f
Matt Miller <thisismattmiller@gmail.com>
2016-01-25 17:20:53 -0500

    pd willa

---
+++ jobs/pd_export_willa.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+var serialize = require("../lib/serialize_utils_pd_willa.js")
+
+
+serialize.serializeMmsItem(function(err,results){
+	serialize.serializeMmsCollection(function(err,results){
+
+	})
+})
